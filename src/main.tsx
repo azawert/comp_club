c02c03eb184eeca570907151023644b170f62c99
azawert <53710085+azawert@users.noreply.github.com>
2023-10-31 16:52:30 +0300

    feat: add i18next and folders for translations

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import '@mantine/core/styles.css'
 import './index.css'
+import './shared/config/i18n/i18n.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
